faeb71ef0d409bac4f412b4fe8b93f31f5556909
Ananda <108600010+0xAnanda@users.noreply.github.com>
2022-09-30 09:06:37 +0800

    add pd.DataFrame methon
    pd.DataFrame

--- week2/Assignment.py
+++ week2/Assignment.py
@@ -76,6 +76,60 @@ print("=========================================================================
 
 
 
+
+
+
+
+# 完成以下函式，正確計算出非 manager 的員工平均薪資，所謂非 manager 就是在資料中manager 欄位標註為 False (Python) 
+# 的員工，程式需考慮員工資料數量不定的情況。
+import pandas as pd
+
+def avg(data):
+    result = 0
+    count = 0
+    datas= pd.DataFrame(data , index=["A", "B", "C", "D"])
+    names = datas["employees"]
+    for right in names:
+        if right["manager"]== False:
+            result += right["salary"]
+            count += 1
+    result /= count
+    print(int(result))
+    return result
+avg({
+
+"employees":[
+{
+"name":"John",
+"salary":30000,
+"manager":False
+},
+{
+"name":"Bob",
+"salary":60000,
+"manager":True
+},
+{
+"name":"Jenny",
+"salary":50000,
+"manager":False
+},
+{
+"name":"Tony",
+"salary":40000,
+"manager":False
+}
+]
+}) # 呼叫 avg 函式
+
+
+
+
+print("=================================================================================")
+
+
+
+
 def func(a):
     def func2(b , c):
         print(a + (b * c))
