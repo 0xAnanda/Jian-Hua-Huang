a7e3f26356db55a8f2989a02f4ecbbc8f04c0456
Ananda <108600010+0xAnanda@users.noreply.github.com>
2023-04-09 15:20:11 +0800

    Add files via upload

--- newWeb/index.js
+++ newWeb/index.js
@@ -10,6 +10,7 @@ window.addEventListener("scroll", function () {
   }
 });
 
+//這樣做的效果是，在頁面頂部右下方會出現一個圓形按鈕，當用戶向下滾動頁面時，按鈕會自動顯示，用戶點擊按鈕時，頁面會平滑地滾動回到頂部。
 window.addEventListener("scroll", function () {
   var scrollToTopBtn = document.getElementById("scrollToTopBtn");
   if (window.scrollY > 0) {
@@ -27,3 +28,23 @@ document
       behavior: "smooth",
     });
   });
+
+//綁定按鈕的點擊事件，並把資訊加入購物車：
+
+var addToCartButtons = document.querySelectorAll(".add-to-cart-button");
+
+addToCartButtons.forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    var name = this.parentElement.querySelector("h3").textContent;
+    var type = this.parentElement.classList.contains("course-details")
+      ? "course"
+      : "space";
+    addToCart(name, type);
+  });
+});
+
+function addToCart(name, type) {
+  // 將商品名稱和類型加入購物車
+  console.log(`Added ${name} (${type}) to cart.`);
+}
