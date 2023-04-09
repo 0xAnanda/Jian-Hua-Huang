1158da192fe6c7c70497d82504febf3e3c63d768
Ananda <108600010+0xAnanda@users.noreply.github.com>
2023-04-10 01:26:31 +0800

    Add files via upload

--- newWeb/index.js
+++ newWeb/index.js
@@ -51,17 +51,47 @@ function addToCart(name, type) {
 
 //用者點擊"會員"時，下拉式選單就會顯示出"登入"和"註冊"選項。當使用者再次點擊"會員"或選擇其中一個選項時，下拉式選單就會隱藏。
 
-const dropdown = document.querySelectorAll(".dropdown");
+// 獲取所有下拉式選單元素
+const dropdownMenus = document.querySelectorAll(".dropdown-menu");
 
-dropdown.forEach((item) => {
-  const link = item.querySelector("a");
-  const menu = item.querySelector(".dropdown-menu");
-
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    menu.classList.toggle("show");
+// 給每個下拉式選單元素添加click事件監聽器
+dropdownMenus.forEach((menu) => {
+  menu.addEventListener("click", () => {
+    // 隱藏下拉式選單
+    menu.style.display = "none";
   });
 });
 
-var audio = new Audio("your_music_file.mp3");
-audio.play();
+// 獲取所有下拉式選單觸發元素
+const dropdownTriggers = document.querySelectorAll("nav li");
+
+// 給每個下拉式選單觸發元素添加click事件監聽器
+dropdownTriggers.forEach((trigger) => {
+  trigger.addEventListener("click", () => {
+    // 獲取觸發元素對應的下拉式選單
+    const dropdownMenu = trigger.querySelector(".dropdown-menu");
+    // 判斷下拉式選單是否顯示，如果顯示，則隱藏它，否則顯示它
+    if (dropdownMenu.style.display === "block") {
+      dropdownMenu.style.display = "none";
+    } else {
+      dropdownMenu.style.display = "block";
+    }
+  });
+});
+
+//設定導航列滾動時變透明
+let header = document.querySelector("header");
+let achor = document.querySelectorAll("header nav ul li a");
+window.addEventListener("scroll", () => {
+  if (window.scrollY != 0) {
+    header.style.backgroundColor = "rgba(0,0,0,0.2)";
+    achor.forEach((a) => {
+      a.style.color = "white";
+    });
+  } else {
+    header.style = "";
+    achor.forEach((a) => {
+      a.style.color = "white";
+    });
+  }
+});
